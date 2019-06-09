64755949069abb9124a0b4eb2b400ec8430a257b
ilia <ilyakut@gmail.com>
2019-06-09 23:50:39 +0300

    "delete" button

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,19 @@
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
   },
-  data () {
-    return {
-      //
-    }
+  data: () => ({}),
+  methods: {
+    ...mapActions(["getData"])
+  },
+  mounted: function() {
+    this.getData();
   }
 }
 </script>
